cde38aafdf4c4465a203c68c147dbbf30ea76815
E-Zbox <zboxstudios97@gmail.com>
2022-08-30 14:16:29 +0100

    Updated backend code
    -	Backend code accepts files uploaded successfully
    -	An error is still being raised when handling files, I think the
    	error has to do with the extractive_summarizer [LINE: 31]

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,26 +1,33 @@
 from flask import Flask, request, jsonify, json
 from utils import extractive_summarizer
-#from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
-#CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True)
 
 
 @app.route("/summary", methods=["POST"])
 def summarizer():
     req_type = request.form['type']
-    body = request.form['body']
     summary_type = request.form['summary_type']
 
-    if req_type == "input_text":
+    if (req_type != "image"):
+        body = request.form['body']
+    else:
+        body = request.files['body']
+        print(dir(body))
+    #print(dir(request))   
+    print({"body":body, "summary_type":summary_type, "type":req_type})
+
+    if req_type == "text":
         summary = extractive_summarizer(input_text = body, summary_type = summary_type)
         
-    elif req_type == "input_link":
+    elif req_type == "link":
         summary = extractive_summarizer(input_link = body, summary_type = summary_type)
         # Input link hasn't be implemented yet, sometimes later
 
-    elif req_type == "input_image":
+    elif req_type == "image":
         summary = extractive_summarizer(input_image=body, summary_type=summary_type)
 
     if summary is not None:
@@ -33,6 +40,7 @@ def summarizer():
         'success': status,
         'model': "extractive"
     }
+    print(jsonResponse)
 
     return jsonResponse
 
